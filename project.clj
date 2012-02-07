44a65502960c8067c3a4a3e0d93cc5b94453c538
Jason Dufair <jase@dufair.org>
2012-02-07 17:53:42 -0500

    initial development

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject testing "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-time "0.3.4"]]
+  :dev-dependencies [[swank-clojure "1.4.0"]]
+  :main testing.core)
---
+++ test/testing/test/core.clj
@@ -0,0 +1,6 @@
+(ns testing.test.core
+  (:use [testing.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
